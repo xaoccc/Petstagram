9b28415af1a5485bce0ac43eb22c13e1386321bb
xaoccc <xaocccc@gmail.com>
2024-01-24 14:14:53 +0200

    setup accounts.urls

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('petstagram.accounts.urls')),
     path('', include('petstagram.common.urls')),
+    path('accounts/', include('petstagram.accounts.urls')),
     path('pets/', include('petstagram.pets.urls')),
     path('photos/', include('petstagram.photos.urls')),
 ]
