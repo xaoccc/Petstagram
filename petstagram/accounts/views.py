24b63cf6413c5ba596670e21baa656df6dbbb2be
xaoccc <xaocccc@gmail.com>
2024-01-24 13:47:18 +0200

    setup accounts.urls

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    return render(request, 'register')
+
+def login(request):
+    return render(request, 'login')
+
+def show_profile_details(request):
+    return render(request, 'profile-details')
+
+def edit_profile(request):
+    return render(request, 'profile-edit')
+
+def delete_profile(request):
+    return render(request, 'profile-delete')
+
+
