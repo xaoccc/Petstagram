413c3f35128bde1ddf9018e1d5f269992e362ab4
xaoccc <xaocccc@gmail.com>
2024-01-26 17:18:14 +0200

    like functionality, requirements.txt

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -4,5 +4,6 @@ from petstagram.common import views
 
 urlpatterns = (
     path('', views.home_page, name='home-page'),
-    path('404/', views.error_404, name='error_404')
+    path('404/', views.error_404, name='error_404'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like')
 )
--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from petstagram.common.models import PhotoLike
 from petstagram.photos.models import Photo
 
 
@@ -14,3 +15,17 @@ def home_page(request):
 
 def error_404(request):
     return render(request, '404.html')
+
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    liked_object = PhotoLike.objects.filter(to_photo_id=photo_id)
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = PhotoLike(to_photo=photo)
+        like.save()
+
+
+    return redirect(request.META['HTTP_REFERER'] + f'#{photo_id}')
