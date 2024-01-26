4609e38e3dab99170c790c7e505e004c0b4eb028
xaoccc <xaocccc@gmail.com>
2024-01-26 15:12:41 +0200

    Dynamically loading of photos on home page (a big struggle with media url settings)

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +28,6 @@ urlpatterns = [
     path('pets/', include('petstagram.pets.urls')),
     path('photos/', include('petstagram.photos.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
