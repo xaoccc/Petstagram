361194beba05fa0586c3d68d85a747ef0ab7c8ba
xaoccc <xaocccc@gmail.com>
2024-02-01 17:30:39 +0200

    photo add with option of adding multiple tagged pets

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -18,5 +18,5 @@ class Pet(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Pet {self.name}"
+        return self.name
 
--- petstagram/photos/forms.py
+++ petstagram/photos/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from petstagram.photos.models import Photo, Pet
 
 class PhotoCreateForm(forms.ModelForm):
-    tagged_pets = forms.ModelMultipleChoiceField(queryset=Pet.objects.all(), widget=forms.CheckboxSelectMultiple)
+    tagged_pets = forms.ModelMultipleChoiceField(queryset=Pet.objects.all(), widget=forms.CheckboxSelectMultiple, label="Tag Pets")
     class Meta:
         model = Photo
         fields = ['photo', 'description', 'location', 'tagged_pets']
