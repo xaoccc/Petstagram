abc9cff967ba1a8ed1968b24dd6de21babb18045
xaoccc <xaocccc@gmail.com>
2024-03-08 14:24:57 +0200

    updatd user model admin panel

--- petstagram/accounts/admin.py
+++ petstagram/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'is_staff', 'is_superuser']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'groups',
+                                    'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+
+
